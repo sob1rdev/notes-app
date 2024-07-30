a979dd3e120323c2387b13b33a1ae2c11964fe78
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-07-30 13:38:55 +0500

    HW

--- src/Api.php
+++ src/Api.php
@@ -1,8 +1,8 @@
 <?php
 
-declare(strict_types=1);
+require 'src/DB.php';
 
-class Notes
+class API
 {
     private PDO $pdo;
 
--- src/DB.php
+++ src/DB.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 class DB
 {
     public static function connect(): PDO
